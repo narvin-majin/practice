7667b0db8d650bacfc2336cd96e398216b6aa428
narvin-majin <narvin.majin@gmail.com>
2026-04-04 20:21:29 +0530

    added while for multi operand, decimal and %

--- projects/calculator-exp/calculator.py
+++ projects/calculator-exp/calculator.py
@@ -1,20 +1,16 @@
 def tokenize(exp):
     token = []
-    temp_num=""
-    operator_allowed="/*-+"
+    temp_num = ""
+    operator_allowed = "/*-+%"
     for ch in exp:
         if ch == " ":
             continue
 
-        if ch.isdigit():
+        if ch.isdigit() or ch == ".":
             temp_num += ch
         
         elif ch in operator_allowed:
-            if not token and temp_num == "":
-                print("Invalid: cannot start with operator")
-                return None
-
-            if token and token[-1] in operator_allowed:
+            if temp_num == "" and token and token[-1] in operator_allowed:
                 print("Invalid: two operators in a row")
                 return None
             
@@ -22,7 +18,7 @@ def tokenize(exp):
                 token.append(temp_num)
 
             token.append(ch)
-            temp_num=""
+            temp_num = ""
         
         else:
             print('invalid input')
@@ -32,26 +28,61 @@ def tokenize(exp):
     return token
 
 def evaluate(tokens):
-    a = int(tokens[0])
-    op = tokens[1]
-    b = int(tokens[2])
+    temp_tokens = []
+    i = 0
 
-    if op == "+":
-        return a + b
-    elif op == "-":
-        return a - b
-    elif op == "/" and b == 0:
-        print("cannot divide by zero")
-        return None
-    elif op == "/":
-        return a / b
-    elif op == "*":
-        return a * b
+    while i < len(tokens):
+        if tokens[i] in "*/":
+            operand_1 = float(temp_tokens.pop())
+            operand_2 = float(tokens[i+1])
+
+            if tokens[i] == "*":
+                temp_tokens.append(operand_1 * operand_2)
+
+            elif tokens[i] == "/":
+                if operand_2 == 0 :
+                    print("Cannot divide by zero")
+                    return None
+                temp_tokens.append(operand_1 / operand_2)
+            
+            elif tokens[i] == "%":
+                temp_tokens.append(operand_1 % operand_2)               #  % is modulos
+            
+            i += 2
+
+        else:
+            temp_tokens.append(tokens[i])
+            i += 1
+
+
+
+    result = float(temp_tokens[0])
+    i = 1
+    while i < len(temp_tokens):
+        operator = temp_tokens[i]
+        operand = float(temp_tokens[i+1])
+
+        if operator == "+":
+            result += operand
+        elif operator == "-":
+            result -= operand
+
+        i += 2 
+    
+    if result.is_integer():
+        result = int(result)
+    
+    return result
+
 
 x = str(input().strip())
 tokens=tokenize(x)
-print(tokens)
-print(evaluate(tokens))
+
+if tokens is None:
+    print("Fix your input")
+else:
+    print(tokens)
+    print(evaluate(tokens))
 
 
 
